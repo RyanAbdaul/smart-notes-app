3d7e5aa8547994b4c81d8610d244efadf428d539
Ryan Abdulaziz <ryanabdaul@gmail.com>
2026-09-18 08:48:06 +0300

    Add authoroties

--- backend/src/main/java/com/smartnotes/app/backend/service/AuthenticationServiceImpl.java
+++ backend/src/main/java/com/smartnotes/app/backend/service/AuthenticationServiceImpl.java
@@ -13,13 +13,11 @@ import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
@@ -49,12 +47,15 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = userRepository.findUserByEmail(input.getEmail())
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
         String token = jwtService.generateToken(new HashMap<>(), user);
-        
-        // Get user's primary role (first authority)
-        // String role = user.getAuthorities().isEmpty() ? "ROLE_USER" 
-        //         : user.getAuthorities().get(0).getAuthority();
-        
-        return new LoginResponse(token);
+
+        Collection<? extends GrantedAuthority> roles = user.getAuthorities();
+        String role = roles.stream()
+                .map(GrantedAuthority::getAuthority)
+                .filter(r -> r.equals(Role.ROLE_ADMIN.name()))
+                .findFirst()
+                .orElse(Role.ROLE_USER.name());
+
+        return new LoginResponse(token, role);
     }
 
     private boolean isEmailTaken(String email) {
